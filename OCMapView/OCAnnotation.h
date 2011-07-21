ac251621da1b70aadb24f8848dc19d6283bb47a8
Botond Kis <boti.kis@gmx.at>
2011-07-21 22:35:24 +0200

    Added OCMapView

---
+++ OCMapView/OCAnnotation.h
@@ -0,0 +1,81 @@
+//
+//  OCAnnotation.h
+//  openClusterMapView
+//
+//  Created by Botond Kis on 14.07.11.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+
+
+/// Annotation class which represents a Cluster.
+/** OCAnnotation stores all annotations which are in its area.
+ Objects of this class will be returned by the delegate method of OCMapView "viewForAnnotation".
+ Implements MKAnnotation protocol.
+ */
+@interface OCAnnotation : NSObject <MKAnnotation>{
+    NSMutableArray *annotationsInCluster;
+    NSString *title;
+    NSString *subtitle;
+    CLLocationCoordinate2D coordinate;
+}
+//
+// Constructors
+/// Standard initializer
+- (id)init;
+
+/// Init with annotations.
+/** Init object with containing annotations*/
+- (id)initWithAnnotation:(id <MKAnnotation>) annotation;
+
+//
+/// List of annotations in the cluster.
+/** Returns all annotations in the cluster.
+ READONLY
+ */
+@property(nonatomic, readonly) NSArray *annotationsInCluster;
+//
+/// List of annotations in the cluster.
+/** @See annotationsInCluster property*/
+- (NSArray *)annotationsInCluster;
+
+//
+// manipulate cluster
+/// Adds a single annotation to the cluster.
+/** Adds a given annotation to the cluster and sets the title to the number of containing annotations.*/
+- (void)addAnnotation:(id < MKAnnotation >)annotation;
+
+/// Adds multiple annotations to the Cluster.
+/** Adds multiple annotations to the cluster and sets the title to the number of containing annotations.
+ Calls addAnnotation in a loop.*/
+- (void)addAnnotations:(NSArray *)annotations;
+
+///Removes a single annotation from the Cluster.
+/** Removes a given annotation from the cluster and sets the title to the number of containing annotations.*/
+- (void)removeAnnotation:(id < MKAnnotation >)annotation;
+
+/// Removes multiple annotations from the Cluster.
+/** Removes multiple annotations from the cluster and sets the title to the number of containing annotations.*/
+- (void)removeAnnotations:(NSArray *)annotations;
+
+//
+// protocoll implementation
+/// Get the Title of the cluster.
+- (NSString *)title;
+/// Set the Title of the cluster.
+- (void)setTitle:(NSString *)text;
+
+
+/// Get the Subtitle of the cluster.
+- (NSString *)subtitle;
+/// Set the Subitle of the cluster.
+- (void)setSubtitle:(NSString *)text;
+
+
+/// Get the coordinate of the cluster.
+- (CLLocationCoordinate2D)coordinate;
+/// Set the coordinate of the cluster.
+- (void)setCoordinate:(CLLocationCoordinate2D)coord;
+
+@end
---
+++ OCMapView/OCMapView.h
@@ -0,0 +1,115 @@
+//
+//  OCMapView.h
+//  openClusterMapView
+//
+//  Created by Botond Kis on 14.07.11.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+#import "OCDistance.h"
+#import "OCAnnotation.h"
+#import "OCAlgorithms.h"
+
+/// MapView which should be used instead of MKMapView
+/** OCMapView works like the standard MKMapView but creates clusters form its containing Annotations.*/
+@interface OCMapView : MKMapView{
+    // Data
+    NSMutableSet *allAnnotations;
+    NSMutableSet *annotationsToIgnore;
+    
+    // Clustering
+    // read properties for explanation
+    BOOL clusteringEnabled;
+    OCClusteringMethod clusteringMethod;
+    CLLocationDistance clusterSize;
+    CLLocationDegrees minLongitudeDeltaToCluster;
+}
+
+// ======================================
+// Overwrite the methods of the mapview
+
+/// Adds the specified annotation to the map view.
+/** The annotation object to add to the receiver. This object must conform to the MKAnnotation protocol.*/
+- (void)addAnnotation:(id < MKAnnotation >)annotation;
+
+/// Adds an array of annotations to the map view.
+/** An array of annotation objects. Each object in the array must conform to the MKAnnotation protocol.*/
+- (void)addAnnotations:(NSArray *)annotations;
+
+/// Removes the specified annotation object from the map view.
+/** The annotation object to remove. This object must conform to the MKAnnotation protocol.*/
+- (void)removeAnnotation:(id < MKAnnotation >)annotation;
+
+/// Removes the specified annotation objects from the map view.
+/** The array of annotations to remove. Objects in the array must conform to the MKAnnotation protocol.*/
+- (void)removeAnnotations:(NSArray *)annotations;
+
+
+// ======================================
+// Properties
+
+//
+/// The complete list of annotations associated with the receiver. (read-only)
+/** The objects in this array must adopt the @see MKAnnotation protocol. If no annotations are associated with the map view, the value of this property is nil.*/
+@property(nonatomic, readonly) NSArray *annotations;
+- (NSArray *)annotations;
+
+//
+/// List of annotations which will be ignored by the clustering algorithm.
+/** The objects in this array must adopt the @see MKAnnotation protocol.
+ The clustering algorithms will automatically ignore this annotations.*/
+@property(nonatomic, strong) NSMutableSet *annotationsToIgnore;
+
+//
+/// The complete list of annotations displayed on the map including clusters (read-only).
+/** The objects in this array must adopt the @see MKAnnotation protocol. It contains all annotations as they are on the MapView.*/
+@property(nonatomic, readonly) NSArray *displayedAnnotations;
+- (NSArray *)displayedAnnotations;
+
+//
+/// enables or disables clustering.
+/** Setting this property will automatical call doClustering
+ @see doClustering.
+ 
+ default: YES*/
+@property(nonatomic, assign) BOOL clusteringEnabled;
+- (void)setClusteringEnabled:(BOOL)enabled;
+
+//
+/// Defines the clustering algorithm which should be used.
+/** @see OCClusteringMethod for more information
+ 
+ default: OCClusteringMethodBubble*/
+@property(nonatomic, assign) OCClusteringMethod clusteringMethod;
+
+
+//
+/// Defines the cluster size in units of the map width.
+/** eg. clusterSize 0.5 is the half of the map.
+default: 0.2*/
+@property(nonatomic, assign) CLLocationDistance clusterSize;
+
+//
+/// Defines the "zoom" from where the map should start clustering.
+/** If the map is zoomed below this value it won't cluster.
+ default: 0.0 (no min. zoom)*/
+@property(nonatomic, assign) CLLocationDegrees minLongitudeDeltaToCluster;
+
+// ======================================
+// Clustering
+
+/// Start the clustering of annotations.
+/**
+ Handles the ignoreList of annotations, calls the defined clustering algorithm and adds the clustered annotations to the map.
+ */
+- (void)doClustering;
+
+// ======================================
+// Help Methods
+
+/// Filters annotations for visibleMapRect.
+/** This method filters the annotations for the visibleMapRect.*/
+- (NSArray *)filterAnnotationsForVisibleMap:(NSArray *)annotationsToFilter;
+
+@end
