d634b991b6c8456ff6449affeaee5247b87706f8
Fedya Skitsko <fskitsko@waverleysoftware.com>
2012-10-24 15:51:01 +0300

    Update for gitignore. Supporting xCode 4

---
+++ OCMapView/OCAnnotationProtocol.h
@@ -0,0 +1,13 @@
+//
+//  OCAnnotationProtocol.h
+//  OClusterMapView+Sample
+//
+//  Created by Skitsko Fedya on 24.10.12.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol OCAnnotationProtocol <NSObject>
+
+@end
