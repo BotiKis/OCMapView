495f23c008119d1e3665e76a84b1beb8c6cf532e
Fedya Skitsko <fskitsko@waverleysoftware.com>
2012-10-24 15:51:17 +0300

    New OCAnnotationProtocol

--- OCMapView/OCAnnotationProtocol.h
+++ OCMapView/OCAnnotationProtocol.h
@@ -1,13 +1,19 @@
 //
-//  OCAnnotationProtocol.h
+//  OCGrouping.h
 //  OClusterMapView+Sample
 //
-//  Created by Skitsko Fedya on 24.10.12.
-//
+//  Created by Fedya Skitsko on 13.02.12.
+//  Copyright (c) 2012 Skitsko. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
+/// Protocol which is needed to use clusters
+/** Implement this protocol to in an annotation to enable cluster*/
 @protocol OCAnnotationProtocol <NSObject>
+- (NSArray *)annotationsInCluster;
+- (void)addAnnotation:(id < MKAnnotation >)annotation;
 
+- (CLLocationCoordinate2D)coordinate;
+- (void)setCoordinate:(CLLocationCoordinate2D)coord;
 @end
