1f9d58fab2c245f5bca4966fd66abbea8581c797
Markus Emrich <Markus.Emrich@gmail.com>
2013-09-24 19:14:31 +0200

    added equality check to example pins

--- OClusterMapView+Sample/OCMapViewSampleHelpAnnotation.m
+++ OClusterMapView+Sample/OCMapViewSampleHelpAnnotation.m
@@ -18,4 +18,19 @@
     return self;
 }
 
+#pragma mark equality
+
+- (BOOL)isEqual:(OCMapViewSampleHelpAnnotation*)annotation;
+{
+    if (![annotation isKindOfClass:[OCMapViewSampleHelpAnnotation class]]) {
+        return NO;
+    }
+    
+    return (self.coordinate.latitude == annotation.coordinate.latitude &&
+            self.coordinate.longitude == annotation.coordinate.longitude &&
+            [self.title isEqualToString:annotation.title] &&
+            [self.subtitle isEqualToString:annotation.subtitle] &&
+            [self.groupTag isEqualToString:annotation.groupTag]);
+}
+
 @end
